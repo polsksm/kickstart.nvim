7397e4462b379b1671276f6196961b9eefdf092e
kmueller <stlmuelk@gmail.com>
2024-01-29 08:51:35 -0600

    Added telescope keymap for "keymaps"

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<C-a>", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-a>", mark.add_file, { desc = 'Harpoon add file' })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = 'Harpoon toggle quick menu' })
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
