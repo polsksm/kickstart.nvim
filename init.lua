c87aaf73981c8f44800778e6e3d08706e630aaa7
kmueller <stlmuelk@gmail.com>
2024-11-30 22:46:01 -0600

    added neogit

--- init.lua
+++ init.lua
@@ -99,6 +99,20 @@ require('lazy').setup({
       require("telescope").load_extension("lazygit")
     end,
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "echasnovski/mini.pick",         -- optional
+    },
+    config = true
+  },
+
   --[[
   {
     "kdheepak/lazygit.nvim",
